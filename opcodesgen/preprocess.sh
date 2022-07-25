0f583a9b3ac1060a06d92e3b5f990ec01fc3de9a
Jan Vrany <jan@vrany.io>
2022-07-25 14:19:11 +0100

    Add swap/reverse/compare/if-compare opcodes to opcode definition
    In order to allow opcodes to cross-reference each other, they're are
    now instantiated in two phases. Also, new `invalid` opcode has been
    added.

--- opcodesgen/preprocess.sh
+++ opcodesgen/preprocess.sh
@@ -22,6 +22,7 @@ sed -e 's#\(   /\* \.description          = \)   #\1*/ "#g' \
     -e 's# \*/ \\$#" \\#g' \
     -e 's#),$#) ,#g' \
     -e 's#NoChildren,#NoChildren ,#g' \
+    -e 's#TR::\([A-Za-z]\)#\1#g' \
     -e 's#\([A-Za-z0-9]\)::\([A-Za-z]\)#\1 \2#g' \
     -e 's#branch targets"#branch targets)"#g' \
     -e 's#case nodes"#case nodes)"#g' \
