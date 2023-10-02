7d0d32a69ffb1ef431e42a8430c1f677fa644ca6
Jan Vrany <jan.vrany@labware.com>
2023-10-02 13:42:42 +0100

    Refactor compilation test shell
    ...to (i) initialize "n-zone" with just return to allow testing
    shell from command line without actually injecting any code and
    (ii) call injected code via call trampoline to allow setting breakpoint
    in GDB just before the call.

--- shell/shell.c
+++ shell/shell.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
 #define NZONE_SIZE 512
 
+#if defined(__x86_64)
+#  define ASM_RETURN "mov %rdi, %rax\nret"
+#elif defined(__riscv)
+#  define ASM_RETURN "ret"
+#elif defined(__powerpc64__)
+#  define ASM_RETURN "blr"
+#else
+#  error "Not (yet) supported architecture"
+#endif
+
 __asm__(
 	"                                  \n"
 	".section .nzone,\"awx\", @progbits\n"
 	"nzone:                            \n"
+	ASM_RETURN "                       \n"
 	".space " TOSTRING(NZONE_SIZE) "   \n"
 	"                                  \n"
 	".section .text                    \n"
@@ -17,9 +29,26 @@ __asm__(
 
 extern unsigned char nzone[NZONE_SIZE];
 
-typedef int (*entry_func)();
+typedef int (*entry_func)(int a);
 static entry_func entry = (entry_func)(&nzone);
 
-int main() {
-	return entry();
+static int __attribute__ ((noinline)) trampoline(int a) {
+	return entry(a);
+}
+
+int main(int argc, char** argv) {
+	int x = 0;
+	switch (argc) {
+		case 1:
+			x = 42;
+			break;
+		case 2:
+			x = strtol(argv[1], NULL, 10);
+			if (errno) return 127;
+			break;
+		default:
+			 return 127;
+			 break;
+	}
+	return trampoline(x);
 }
